a15d51cd36089c1262078f820841cd6da00d001d
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-01-09 00:22:20 +0000

    SDK regeneration

--- src/merge/resources/ticketing/types/remote_data.py
+++ src/merge/resources/ticketing/types/remote_data.py
@@ -13,7 +13,7 @@ except ImportError:
 
 class RemoteData(pydantic.BaseModel):
     path: str
-    data: typing.Optional[typing.Dict[str, typing.Any]]
+    data: typing.Optional[typing.Any]
 
     def json(self, **kwargs: typing.Any) -> str:
         kwargs_with_defaults: typing.Any = {"by_alias": True, "exclude_unset": True, **kwargs}
